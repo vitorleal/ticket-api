caf6f9f5ebd50659ce334c43146864134c57940f
Vitor Leal <vitorleal1@gmail.com>
2013-11-25 17:43:40 -0200

    added max-age to the request header

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ var def = {
 var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Cache-Control:", "max-age=0");
   next();
 };
 
